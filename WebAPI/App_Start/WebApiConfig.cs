b85ab0a499025f26e26feabb8170d61c4633ea3e
Ray <jackboy1006@gmail.com>
2021-02-20 10:36:35 +0800

    加入專案檔案。

---
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebAPI
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API 設定和服務
+
+			// Web API 路由
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi3",
+				routeTemplate: "api/{controller}/{a}/{b}",
+				defaults: new { id = RouteParameter.Optional }
+				//a 與 b 兩個變數名稱 需要跟 觸發方法的輸入參數一樣!!
+			);
+		}
+	}
+}
---
+++ WebAPI/Controllers/WebAPI2Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class WebAPI2Controller : ApiController
+    {
+        [HttpGet]//建議寫 雖然API會自動識別 但有寫幫助自己辨別
+        public string GetHello()
+		{
+            return "Hello";
+		}
+
+        [HttpGet]//建議寫 雖然API會自動識別 但有寫幫助自己辨別
+        public string GetHelloWorld(int id)
+		{
+            return "Hello! The World is ..." + id.ToString();
+		}
+
+        [HttpGet]//建議寫 雖然API會自動識別 但有寫幫助自己辨別
+        public string GetComputeIT(int a,int b)
+		{
+            int result = a + b;
+            return result.ToString();
+        }
+
+    }
+}
